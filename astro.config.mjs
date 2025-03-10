440649ed49daa43aac7448e48bc868cfc4bf27a9
Peter <139961432+peterktsitsi@users.noreply.github.com>
2025-03-10 13:46:55 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-sphere-demo.vercel.app",
+  site: 'https://peterktsitsi.github.io',
+  base: 'https://github.com/peterktsitsi/Astro-Personal-Site',
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
